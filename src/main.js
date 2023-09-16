5a30e4e3963e5e4c2e752cb294922e2982797123
青旬 <947338658@qq.com>
2023-09-16 19:11:54 +0800

    修改项目架构，添加router

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import router from './router';
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
-router.beforeEach((to, from, next) => {
-    /* 路由发生变化修改页面title */
-    if (to.meta.title) {
-        document.title = to.meta.title
-    }
-    next()
-})
-
 new Vue({
-	router,
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="home">
+	  
+	  <el-row>
+	    <el-col :span="24"><div class="grid-content bg-purple-dark"><BlogMenu></BlogMenu></div></el-col>
+	  </el-row>
+	  <el-row :gutter="2">
+	    <el-col :span="16">
+			<div class="grid-content bg-purple">
+				<BlogBanner></BlogBanner>
+				<BlogDetails></BlogDetails>
+				<BlogEssayList></BlogEssayList>
+			</div>
+		</el-col>
+	    <el-col :span="8">
+			<div class="grid-content bg-purple-light">
+				<BlogHandpick></BlogHandpick>
+				<BlogDate></BlogDate>
+				<BlogIntroduction></BlogIntroduction>
+			</div>
+		</el-col>
+	  </el-row>
+	  <el-row>
+	    <el-col :span="24"><div class="grid-content bg-purple-dark"><BlogNavigation></BlogNavigation></div></el-col>
+	  </el-row>
+
+  </div>
+</template>
+
+<script>
+import BlogMenu from '../components/BlogMenu.vue'
+import BlogBanner from '../components/BlogBanner.vue'
+import BlogHandpick from '../components/BlogHandpick.vue'
+import BlogDate from '../components/BlogDate.vue'
+import BlogDetails from '../components/BlogDetails.vue'
+import BlogEssayList from '../components/BlogEssayList.vue'
+import BlogIntroduction from '../components/BlogIntroduction.vue'
+import BlogNavigation from '../components/BlogNavigation.vue'
+
+export default {
+  name: 'home',
+  components: {
+	BlogMenu,
+	BlogBanner,
+	BlogHandpick,
+	BlogDate,
+	BlogDetails,
+	BlogEssayList,
+	BlogIntroduction,
+	BlogNavigation
+  }
+}
+</script>
+
+<style>
+#home {
+  color: #2c3e50;
+  min-width: 1350px; /* 最小宽度 */
+}
+</style>
