7c650e6e71df9069dabf76f7edcd75c836cef04d
青旬 <947338658@qq.com>
2023-09-16 17:56:39 +0800

    添加了router，但是报错

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import router from './router';
+import router from './router';
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
@@ -16,6 +16,6 @@ router.beforeEach((to, from, next) => {
 })
 
 new Vue({
-	// router,
+	router,
   render: h => h(App),
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-// import Vue from 'vue'
-// import VueRouter from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-// Vue.use(VueRouter);
+Vue.use(VueRouter);
 
-// const routes = [
-//     {
-//       path: '/',
-//       name: 'Home',
-//       component: Home
-//     },
-//     {
-//       path: '/article/:id', // 这里定义了文章详情的路由
-//       name: 'ArticleDetail',
-//       component: ArticleDetail
-//     },
-// ]
+const routes = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/article/:id', // 这里定义了文章详情的路由
+      name: 'ArticleDetail',
+      component: ArticleDetail
+    },
+]
 
-// const router = new VueRouter({
-//     routes
-// })
+const router = new VueRouter({
+    routes
+})
 
-// export default router
+export default router
