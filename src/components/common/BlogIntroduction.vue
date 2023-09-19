b2dcc7208fbec5770a2d572b1128ec4d3a2e9398
青旬 <947338658@qq.com>
2023-09-20 04:40:34 +0800

    修复部分漏洞

--- src/components/common/BlogIntroduction.vue
+++ src/components/common/BlogIntroduction.vue
@@ -2,7 +2,7 @@
 	<div id="BlogIntroduction">
 		<el-card class="Introduction-card">
 		  <div slot="header" class="clearfix">
-		    <span style="font-size: 20px; color: red;">欢迎来到 QxBase博客</span>
+		    <span style="font-size: 20px; color: red;">欢迎来到 qxBase博客</span>
 		  </div>
 		  <div class="text item">
 		    我们的目标是为您提供有关计算机科学和实用业务问题的清晰、实用和有趣的信息。请随时浏览我们的博客，参与我们的社区，与我们分享您的想法和反馈。我们致力于计算机和实用业务解决方案的分享
