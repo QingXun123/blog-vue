da1c44b54dec7ac0446a51759c80dbd5bc1afcb8
青旬 <947338658@qq.com>
2023-09-20 22:58:41 +0800

    添加搜索框

--- src/components/mobile/common/MobileMenu.vue
+++ src/components/mobile/common/MobileMenu.vue
@@ -1,8 +1,10 @@
 <template>
 	<div id="MobileMenu">
 		<!-- 菜单按钮 -->
-		<el-button @click="openDrawer" icon="el-icon-menu" style="width: 50px; height: 50px; display: flex; align-items: center; justify-content: center; font-size: 1em; border: none;"></el-button>
-	
+		<el-button @click="openDrawer" icon="el-icon-menu" style="width: 60px; height: 100%; align-items: center; justify-content: center; font-size: 1em; border: none;"></el-button>
+		<!-- 搜索按钮 -->
+		<el-button @click="openSearch" icon="el-icon-search" style="width: 60px; height: 100%; align-items: center; justify-content: center; font-size: 1em; border: none; margin-left: auto;"></el-button>
+		
 		<!-- 左侧弹出菜单栏 -->
 		<el-drawer
 		  title="菜单栏"
@@ -17,6 +19,26 @@
 			<el-menu-item index="4" disabled>服务中心</el-menu-item>
 		  </el-menu>
 		</el-drawer>
+		
+		<!-- 搜索抽屉 -->
+		<el-drawer
+		  title="搜索"
+		  :visible.sync="searchDrawerVisible"
+		  direction="ttb"
+		  :with-header="false"
+		  :size="70">
+		  <div style="justify-content: space-between; display: flex; padding: 15px 0px; justify-content: center;">
+			  <el-input
+			      placeholder="请输入内容"
+			      prefix-icon="el-icon-search"
+			      v-model="input2"
+			  			  style=" width: 80%; flex-direction: row; align-items: center;"
+			  	@keyup.enter.native="redirectToSearch()">
+			    </el-input>
+				<el-button @click="openSearch" icon="el-icon-search" style="width: 40px; height: 40px; display: flex; align-items: center; justify-content: center; font-size: 1em; border: none;"></el-button>
+		  </div>
+		  
+		</el-drawer>
 	</div>
 </template>
 
@@ -24,7 +46,8 @@
 	export default {
 	    data() {
 	        return {
-	          drawerVisible: false, // 控制菜单栏的显示与隐藏
+			  drawerVisible: false, // 控制菜单栏的显示与隐藏
+	          searchDrawerVisible: false, // 控制搜索抽屉的显示与隐藏
 	        };
 	      },
 	      methods: {
@@ -32,6 +55,10 @@
 	          // 打开菜单栏
 	          this.drawerVisible = true;
 	        },
+			openSearch() {
+			    // 打开搜索抽屉
+			    this.searchDrawerVisible = true;
+			  },
 	      },
 	  }
 </script>
@@ -40,5 +67,6 @@
 	#MobileMenu {
 		background-color: white;
 		height: 60px;
+		display: flex;
 	}
 </style>
